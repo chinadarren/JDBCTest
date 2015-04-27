2f823c81ec5202d611404e747d1a2c2d02647c4c
chenchina <chenlei520>
2015-04-27 14:00:20 +0800

    2

--- src/TestJDB.java
+++ src/TestJDB.java
@@ -1,5 +1,8 @@
 import java.sql.*;
 import com.mysql.jdbc.Driver;
+
+import javax.swing.plaf.nimbus.State;
+
 /**
  * Created by lei.chen on 4/27/2015.
  */
@@ -20,34 +23,21 @@ public class TestJDB {
         }
         return null;
     }
-//关闭连接
-    public static void close(ResultSet resultSet,PreparedStatement preparedStatement,Connection connection) {
 
-        if(resultSet !=null){
-            try {
-                resultSet.close();
-            } catch (SQLException e) {
-                e.printStackTrace();
-            }
-        }
-        if ((preparedStatement != null)){
-            try {
-                preparedStatement.close();
-            } catch (SQLException e) {
-                e.printStackTrace();
-            }
-        }
-        if (connection != null){
-            try {
-                connection.close();
-            } catch (SQLException e) {
-                e.printStackTrace();
-            }
-        }
-
-    }
 //输出连接信息
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception{
+        //测试数据库连接
         System.out.println(getConnection());
+        //取出数据
+        Connection conn = getConnection();
+        Statement stmt = conn.createStatement();
+        ResultSet rs = stmt.executeQuery("select * from user");
+        while (rs.next()){
+            System.out.println(rs.getString("id"));
+        }
+        //关闭连接
+        rs.close();
+        stmt.close();
+        conn.close();
     }
 }
